a00727559f39953af956a4f632865b3f0f1217cd
lupindevv <alexxthm1@gmail.com>
2025-02-21 20:48:57 +0200

    add-param-to-docker-build

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo 'Building Docker image...'
     
     withCredentials([usernamePassword(credentialsId: 'dockerhubs', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t alexthm1/demo-app:jma-2.2 .'
+        sh "docker build -t $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push alexthm1/demo-app:jma-2.2'
+        sh "docker push $imageName"
     }
 }
--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def call () {
-    echo 'building the app..'
+    echo "building the app for brach $BRANCH_NAME"
     sh 'mvn package'
 }
